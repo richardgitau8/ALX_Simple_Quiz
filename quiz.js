1582a3bfd1c08136acc89476402bc7f8aa3f5be8
Richard Gitau <162958992+richardgitau8@users.noreply.github.com>
2024-07-11 13:24:26 +0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,22 @@
+
+function checkAnswer() {
+   
+    var correctAnswer = "4";
+
+    var userAnswer = document.querySelector('input[name="quiz"]:checked');
+    
+     if (userAnswer) {
+        
+        if (userAnswer.value == correctAnswer) {
+        
+            document.getElementById('feedback').textContent = "Correct! Well done.";
+        } else {
+           
+            document.getElementById('feedback').textContent = "That's incorrect. Try again!";
+        }
+    } else {
+       
+        document.getElementById('feedback').textContent = "Please select an answer.";
+    }
+}
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
