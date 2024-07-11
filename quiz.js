0c1d1fce2d58c25e79dcbd058cd1787e5e29633d
Richard Gitau <162958992+richardgitau8@users.noreply.github.com>
2024-07-11 14:00:45 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -4,6 +4,11 @@ function checkAnswer() {
     var correctAnswer = "4";
 
     var userAnswer = document.querySelector('input[name="quiz"]:checked');
+   
+   if (userAnswer === correctAnswer) {
+            document.getElementById("feedback").textContent = "Correct! Well done.";
+        } else {
+            document.getElementById("feedback").textContent = "That's incorrect. Try again!";
     
      if (userAnswer) {
         
